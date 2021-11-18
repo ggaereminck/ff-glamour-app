10bed5b52d5120226b774fed5b335ac45e079697
Charlie Gaereminck <ggaereminck@yahoo.com>
2021-11-17 19:55:13 -0500

    added uniqueness to reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,5 @@ class Review < ApplicationRecord
     belongs_to :post
 
     validates :rating, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 5}
+    validates :user_id, uniqueness: { scope: :post_id}
 end
